82dfb47dfeba0cf5b3c56e3c5b24d9b886ef622b
Julissa Huaman Hilari <julissa.2606@gmail.com>
2021-10-05 12:22:49 -0500

    make it better

--- index.js
+++ index.js
@@ -6,3 +6,14 @@ const { port } = require('./config')
 app.use(express.json());
 app.set('port', port);
 app.use(express.urlencoded({ extended: true }));
+
+app.use(require('./routes/routes'));
+
+const server = app.listen(app.get('port'), () => {
+    console.info(`Escuchando el puerto ${app.get('port')}`)
+});
+
+module.exports = {
+    app,
+    server
+};
---
+++ routes/routes.js
@@ -0,0 +1,70 @@
+const { Router } = require('express');
+
+const router = Router();
+
+const { parse, stringify } = require('roman-numerals-api');
+const pkg = require('../package.json');
+router.get('/', (req, res) => {
+    res.json({
+        name: pkg.name,
+        version: pkg.version,
+    });
+});
+
+// POST
+router.post('/', (req, res) => {
+    const { text } = req.body;
+    console.log(text)
+    const [name, param] = text.split(' ');
+  
+    const resultArabigo = (value) => {
+      try {
+        return parse(value);
+      } catch (error) {
+        return error.message;
+      }
+    };
+  
+    const resultRoman = (value) => {
+      try {
+        return stringify(value);
+      } catch (error) {
+        return error.message;
+      }
+    };
+  
+    if (name === 'parse') {
+      res.json({
+        response_type: 'in channel',
+        text: resultArabigo(param),
+      });
+    }
+    if (name === 'stringify') {
+      res.json({
+        response_type: 'in channel',
+        text: resultRoman(+param),
+      });
+    }
+  
+    if (name === 'help') {
+      res.json({
+        response_type: 'in channel',
+        text: 'Convertir de ROMAN-ARA [ ejemplo: /parse 10 ], de ARA-ROMAN: [ ejemplo: /stringify VII ]',
+      });
+    }
+  
+    if (name === 'version') {
+      res.json({
+        response_type: 'in channel',
+        text: pkg.version,
+      });
+    }
+  
+    res.status(400).json({
+      response_type: 'ephemeral',
+      text: "Sorry, slash commando, that didn't work. Please try again.",
+    });
+
+});
+
+module.exports = router;
